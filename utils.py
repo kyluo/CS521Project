742f6a58b2b279d2f0cb76255f243820b9a6e7b0
Kaiyuan Luo <47092950+kyluo@users.noreply.github.com>
2023-11-28 13:51:33 -0600

    Update utils.py
    update loss

--- utils.py
+++ utils.py
@@ -3,13 +3,16 @@ import torch.optim as optim
 from tqdm import tqdm
 import setting
 
+def loss_MSE():
+     return nn.MSELoss()
+
 
 def train(data_loader, model):
     model.train()
     it_train = tqdm(enumerate(data_loader), total=len(
         data_loader), desc="Training ...", position=0, disable=False)
     
-    loss_mse = nn.MSELoss()
+    loss_func = loss_MSE()
     optimizer = optim.Adam(model.paramters(), lr=0.001, weight_decay=0.0001, betas=(0.9, 0.999))
 
     for _, (x, y) in it_train:
@@ -17,19 +20,19 @@ def train(data_loader, model):
 
         optimizer.zero_grad()
         y_pred = model.forward(x)
-        loss = loss_mse(y, y_pred)
+        loss = loss_func(y, y_pred)
         loss.backward()
         optimizer.step()
         
 
 def val(data_loader, model):
-    loss_mse = nn.MSELoss()
+    loss_func = loss_MSE()
     model.eval()
     
     total_loss = 0
     for _, (x, y) in enumerate(data_loader):
         y_pred = model.forward(x)
-        loss = loss_mse(y, y_pred)
+        loss = loss_func(y, y_pred)
         total_loss += loss
     return total_loss
 
